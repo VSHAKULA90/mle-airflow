7b3a390560ce15acd63ffff746659be6aeccc47f
Vadim Shakula <vadim.shakula@internet.ru>
2025-08-16 13:48:59 +0000

    20250816v6

--- plugins/steps/messages.py
+++ plugins/steps/messages.py
@@ -3,26 +3,26 @@ from airflow.providers.telegram.hooks.telegram import TelegramHook
 
 def send_telegram_failure_message(context):
     hook = TelegramHook(
-        token='{8406512077:AAGmVhp8qQ-AQBOJyvazOhyvbCBVEu9Vun4}', 
-        chat_id='{-4935755180}')
+        token='8406512077:AAGmVhp8qQ-AQBOJyvazOhyvbCBVEu9Vun4', 
+        chat_id='-4935755180')
     run_id = context['run_id']
     task_key = context['task_instance_key_str']
     
     message = f'Исполнение Дага завершилось с ошибкой. run_id: {run_id}. задача: {task_key}'
     hook.send_message({
-        'chat_id': '{-4935755180}',
+        'chat_id': '-4935755180',
         'text': message
     })
     
 def send_telegram_success_message(context): #добавил сам, на уроке не было
     hook = TelegramHook(telegram_conn_id='test',
-                        token='{8406512077:AAGmVhp8qQ-AQBOJyvazOhyvbCBVEu9Vun4}',
-                        chat_id='{-4935755180}')
+                        token='8406512077:AAGmVhp8qQ-AQBOJyvazOhyvbCBVEu9Vun4',
+                        chat_id='-4935755180')
     dag = context['dag']
     run_id = context['run_id']
     
     message = f'Исполнение Дага {dag} с run_id: {run_id} прошло успешно!' 
     hook.send_message({
-        'chat_id': '{-4935755180}',
+        'chat_id': '-4935755180',
         'text': message
     })
